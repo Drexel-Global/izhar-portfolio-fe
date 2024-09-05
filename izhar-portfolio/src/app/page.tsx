cf9a5c3cb1219a9bc2ac0fc3fc154b16b3c0f65a
Samuel Torres <rilla@MacBook-Pro.local>
2024-09-05 16:54:24 -0400

    deployment & domain configuration commit

--- izhar-portfolio/src/app/page.tsx
+++ izhar-portfolio/src/app/page.tsx
@@ -1,9 +1,22 @@
 import styles from "./page.module.css";
 
-export default function Home() {
+async function fetchHomePageData() {
+  const res = await fetch("http://localhost:1337/api/homepages?populate=*", {
+    next: { revalidate: 0 },
+  });
+  const data = await res.json();
+  if (res.status !== 200) throw new Error("Failed to fetch data");
+  return data;
+}
+
+export default async function Home() {
+  const data = await fetchHomePageData();
+  console.log("DATA: ", data.data);
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <h1>COMING SOON!</h1>
+      </main>
       <footer className={styles.footer}></footer>
     </div>
   );
